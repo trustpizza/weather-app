107acaaeb1a1c686e27a641ab4105ecb1783091d
trustpizza <maxelolsson@gmail.com>
2023-03-08 14:36:30 -0600

    Setup tailwind for frontend css on app

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,23 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
   module: {
     rules: [
-      {
+    {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',

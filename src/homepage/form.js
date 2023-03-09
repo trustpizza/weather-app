ebbe90a2de132dea2d94adc5abf3ca20742939dd
trustpizza <maxelolsson@gmail.com>
2023-03-09 12:41:14 -0600

    Begin form

--- src/homepage/form.js
+++ src/homepage/form.js
@@ -1,5 +1,20 @@
 const searchBar = () => {
+    const form = document.createElement('form');
+    form.setAttribute("method", "post");
+    form.setAttribute("action", "submit");
+
+    const searchDiv = document.createElement('div');
+
+    const searchbar = document.createElement('input');
     
+    /* 
+        We need a form
+        Form takes an input
+        Input has a hidden submit button? (maybe instead something else)
+        Form searches and finds cities/countries
+    */
+
+    return form
 };
 
 export default searchBar

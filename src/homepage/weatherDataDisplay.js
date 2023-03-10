8e2b4157576c562d4cffcb38fa8b73a4a1e31bce
trustpizza <maxelolsson@gmail.com>
2023-03-10 12:33:23 -0600

    Create next section of weather information and import cloudy svg

--- src/homepage/weatherDataDisplay.js
+++ src/homepage/weatherDataDisplay.js
@@ -1,7 +1,10 @@
-const WeatherDataDisplay = (weatherData) => {
+import Cloudy from "../photos/weather-cloudy.svg"
+
+const WeatherDataDisplay = () => {
   const card = document.createElement("div");
   card.className = "flex flex-col bg-white rounded p-4 w-full max-w-xs";
 
+  // Header of Card
   const cityName = document.createElement("div");
   cityName.textContent = "Boston, MA";
   cityName.className = "font-bold text-xl";
@@ -16,27 +19,72 @@ const WeatherDataDisplay = (weatherData) => {
   weatherIconDiv.ariaHidden = "true";
 
   const weatherIcon = new Image();
+  weatherIcon.src = Cloudy
   weatherIcon.className = "w-32 h-32";
 
   weatherIconDiv.appendChild(weatherIcon);
+  // End Header of Card
 
-  card.append(cityName, date, weatherIconDiv);
+  // Main Temperature Section
 
-  const update = () => {
+  const tempSection = document.createElement('div');
+  tempSection.className = "flex flex-row items-center justify-center mt-6"  
+
+  const temp = document.createElement('div');
+  temp.className = "font-medium text-6xl"
+  temp.textContent = "24°F"
+
+  const rightHalf = document.createElement('div');
+  rightHalf.className = "flex flex-col items-center ml-6"
+
+  const cloudCoverage = document.createElement('div');
+  cloudCoverage.textContent = "Cloudy";
+
+  const highTemp = document.createElement('div');
+  highTemp.className = "mt-1";
+  highTemp.ariaHidden = "true";
+
+  const highTempSpan = document.createElement('span');
+  highTempSpan.textContent = "28°F";
+
+  highTemp.appendChild(highTempSpan);
+
+  const lowTemp = document.createElement('div');
+  lowTemp.className = "mt-1";
+  lowTemp.ariaHidden = "true";
+
+  const lowTempSpan = document.createElement('span');
+  lowTempSpan.textContent = "22°F";
+
+  lowTemp.appendChild(lowTempSpan);
+
+  rightHalf.append(cloudCoverage, highTemp, lowTemp);
+
+  tempSection.append(temp, rightHalf)
+
+  // End Main Temp Section
+
+  // Misc Weather Information
+
+  // End Misc Weather Info
+
+  const update = (data) => {
     const cardObjs = {
-      city: cityName,
+      cityName,
       date,
       weatherIcon,
     };
     populateWeatherDisplay(cardObjs);
   };
 
+  card.append(cityName, date, weatherIconDiv, tempSection);
+
   return { card, update };
 };
 
 function populateWeatherDisplay(objs, data) {
   // update the data
-  objs.city.textContent = "Boston";
+  objs.cityName.textContent = "Boston";
 }
 
 export { WeatherDataDisplay };

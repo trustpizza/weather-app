ca93d10f1273fd4fcea7c97fadcb9c0e3ab7e70c
trustpizza <maxelolsson@gmail.com>
2023-03-12 09:04:11 -0500

    Leave footer for now

--- src/homepage/footer.js
+++ src/homepage/footer.js
@@ -1,15 +1,16 @@
 const footer = () => {
     const container = document.createElement('footer');
-    container.className = "p-4 bg-white absolute bottom-0 w-full rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800"
+    container.className = "p-4 bg-white absolute bottom-0 w-full shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800"
 
     const linkSection = document.createElement('div');
     linkSection.className = "sm:flex sm:items-center sm:justify-between"
     
     const TOPshoutout = document.createElement('span');
-    TOPshoutout.textContent = "© 2023";
+    TOPshoutout.textContent = "© 2023 ";
 
     const TOPlink = document.createElement('a');
     TOPlink.textContent = "The Odin Project";
+
     TOPlink.className = "hover:underline";
     TOPlink.href = "https://www.theodinproject.com/lessons/javascript-weather-app"
 

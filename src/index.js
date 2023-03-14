27199a46a734a01d94b2572b46cea9f12d148920
trustpizza <maxelolsson@gmail.com>
2023-03-14 18:05:01 -0500

    Add favicon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import "./style.css";
 import { homepage } from "./homepage/homepage";
 import Footer from "./homepage/footer";
+import Clouds from "./photos/weather-cloudy.svg"
+
+function setFavicon(image) {
+    const headTitle = document.querySelector('head');
+    const setFavicon = document.createElement('link');
+    setFavicon.setAttribute('rel', 'shortcut icon');
+    setFavicon.setAttribute('href', image);
+    headTitle.appendChild(setFavicon);
+}
+
+setFavicon(Clouds)
+
 
 const content = document.getElementById("content");
 const display = homepage();
